4dabdf3dc4256aece27d05ce86fe6ef73bcf27a2
MaksatRysbekov <maksatrysbekov96@gmail.com>
2021-07-04 10:56:00 +0200

    first commit

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,125 +1,127 @@
-import {useState, useEffect} from 'react'
-import {getAllStudents} from "./client";
-import {
-    Layout,
-    Menu,
-    Breadcrumb,
-    Table, Spin, Empty
-} from 'antd';
-import {
-    DesktopOutlined,
-    PieChartOutlined,
-    FileOutlined,
-    TeamOutlined,
-    UserOutlined,
-    LoadingOutlined
-} from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb, Table, Spin, Alert } from "antd";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import "./App.css";
+import { getAllStudents } from "./client";
 
-const {Header, Content, Footer, Sider} = Layout;
-const {SubMenu} = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 const columns = [
-    {
-        title: 'Id',
-        dataIndex: 'id',
-        key: 'id',
-    },
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
-    },
-    {
-        title: 'Gender',
-        dataIndex: 'gender',
-        key: 'gender',
-    },
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
 ];
 
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
-
 function App() {
-    const [students, setStudents] = useState([]);
-    const [collapsed, setCollapsed] = useState(false);
-    const [fetching, setFetching] = useState(true);
+  const [students, setStudents] = useState([]);
+  const [collapsed, setCollapsed] = useState(false);
+  const [fetching, setFetching] = useState(true);
 
-    const fetchStudents = () =>
-        getAllStudents()
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setStudents(data);
-                setFetching(false);
-            })
+  const fetchStudents = () =>
+    getAllStudents()
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data);
+        console.log(data);
+        setFetching(false);
+      });
 
-    useEffect(() => {
-        console.log("component is mounted");
-        fetchStudents();
-    }, []);
+  useEffect(() => {
+    console.log("component is mounted");
+    fetchStudents();
+  }, []);
 
-    const renderStudents = () => {
-        if (fetching) {
-            return <Spin indicator={antIcon} />
-        }
-        if (students.length <= 0) {
-            return <Empty />;
-        }
-        return <Table
-            dataSource={students}
-            columns={columns}
-            bordered
-            title={() => 'Students'}
-            pagination={{ pageSize: 50 }}
-            scroll={{ y: 240 }}
-        />;
+  const renderStudents = () => {
+    if (fetching) {
+      return (
+        <Spin tip="Loading...">
+          <Alert
+            message="Alert message title"
+            description="Further details about the context of this alert."
+            type="info"
+          />
+        </Spin>
+      );
     }
+    if (students.length <= 0) {
+      return "<Empty/>";
+    }
+    return (
+      <Table
+        dataSource={students}
+        columns={columns}
+        rowKey={(student) => student.id}
+      />
+    );
+  };
 
-    return <Layout style={{minHeight: '100vh'}}>
-        <Sider collapsible collapsed={collapsed}
-               onCollapse={setCollapsed}>
-            <div className="logo"/>
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined/>}>
-                    Option 1
-                </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined/>}>
-                    Option 2
-                </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined/>}>
-                    Files
-                </Menu.Item>
-            </Menu>
-        </Sider>
-        <Layout className="site-layout">
-            <Header className="site-layout-background" style={{padding: 0}}/>
-            <Content style={{margin: '0 16px'}}>
-                <Breadcrumb style={{margin: '16px 0'}}>
-                    <Breadcrumb.Item>User</Breadcrumb.Item>
-                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
-                    {renderStudents()}
-                </div>
-            </Content>
-            <Footer style={{textAlign: 'center'}}>By Amigoscode</Footer>
-        </Layout>
+  if (students.length <= 0) return "<Empty/>";
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(c) => setCollapsed}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            {renderStudents(students)}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Amigoscode ©2021 Created by Maksat R.
+        </Footer>
+      </Layout>
     </Layout>
+  );
 }
 
 export default App;
